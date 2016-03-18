5b4fb45e0fe20ff4ddc140984d7c3124369e14ca
surenhakobyan <surojermuk@gmail.com>
2016-03-18 18:16:41 +0400

    MVCCV

---
+++ MVCCV/MVCCV/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using MVCCV.Models;
+
+namespace MVCCV.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ContactModel model = new ContactModel();
+            ViewBag.Title = "My CV with MVC";
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult SendMail(ContactModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    MailMessage mail = new MailMessage();
+
+                    mail.To.Add("surojermuk@gmail.com");
+                    mail.From = new MailAddress(model.Email);
+                    mail.Subject = model.Subject;
+
+                    mail.Body = String.Concat(model.Message, " - From: ", model.Email, ": ",
+                        model.Name);
+
+                    mail.IsBodyHtml = true;
+                    SmtpClient smtp = new SmtpClient();
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Credentials = new System.Net.NetworkCredential
+                         ("surojermuk@gmail.com", "mypassword");
+                    smtp.Port = 587;
+
+                    smtp.EnableSsl = true;
+                    smtp.Send(mail);
+
+                    return RedirectToAction("Index");
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            else
+                return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ MVCCV/MVCCV/Models/ContactModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCCV.Models
+{
+    public class ContactModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "E-mail is not valid")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Subject is required")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "Message is required")]
+        public string Message { get; set; }
+    }
+}
